b881629f47ce9aa1b0da2439bd4245db104870bc
fonias95531 <137882519+fonias95531@users.noreply.github.com>
2024-03-26 16:02:28 +0500

    9 solved

---
+++ var inf2310301/9/9.py
@@ -0,0 +1,43 @@
+f = open('var inf2310302/9/9.txt')
+c = [{}, {}, {}, {}, {}, {}]
+c1 = f.readline().split()
+while c1 != []:
+    for i in range(6):
+        if c1.count(c1[i])==1:
+            try:
+                c[i][c1[i]]+=1
+            except:
+                c[i][c1[i]]=1
+    c1 = f.readline().split()
+f.close()
+c2 = [[], [], [], [], [], []]
+for i in range(6):
+    for j in c[i]:
+        if j not in c2[i]:
+            c2[i].append(j)
+        c[i][j] = 0
+f = open('var inf2310302/9/9.txt')
+c1 = f.readline().split()
+while c1 != []:
+    for i in range(6):
+        if c1[i] in c2[i]:
+            c[i][c1[i]]+=1
+    c1 = f.readline().split()
+f.close()
+c2 = [[], [], [], [], [], []]
+for i in range(6):
+    for j in c[i]:
+        if c[i][j]>150 and j not in c2[i]:
+            c2[i].append(j)
+f = open('var inf2310302/9/9.txt')
+c1 = f.readline().split()
+rc = 0
+while c1 != []:
+    c = 0
+    for i in range(6):
+        if c1.count(c1[i])==1 and c1[i] in c2[i]:
+            c+=1
+    if c>4:
+        rc+=1
+    c1 = f.readline().split()
+print(rc)
--- var inf2310302/9/9.py
+++ var inf2310302/9/9.py
@@ -1,15 +1,43 @@
 f = open('var inf2310302/9/9.txt')
-c = [{}*6]
+c = [{}, {}, {}, {}, {}, {}]
 c1 = f.readline().split()
 while c1 != []:
-    for i in c1:
-        if c1.count(i)==1:
+    for i in range(6):
+        if c1.count(c1[i])==1:
             try:
-                c[0][c1[0]]+=1
+                c[i][c1[i]]+=1
             except:
-                c[0][c1[0]]=1
-for i in c:
-    for j in i:
-        if i[j]<170:
-            pass
-    
+                c[i][c1[i]]=1
+    c1 = f.readline().split()
+f.close()
+c2 = [[], [], [], [], [], []]
+for i in range(6):
+    for j in c[i]:
+        if c[i][j]<170 and j not in c2[i]:
+            c2[i].append(j)
+            c[i][j] = 0
+f = open('var inf2310302/9/9.txt')
+c1 = f.readline().split()
+while c1 != []:
+    for i in range(6):
+        if c1[i] in c2[i]:
+            c[i][c1[i]]+=1
+    c1 = f.readline().split()
+f.close()
+c2 = [[], [], [], [], [], []]
+for i in range(6):
+    for j in c[i]:
+        if c[i][j]<170 and j not in c2[i]:
+            c2[i].append(j)
+f = open('var inf2310302/9/9.txt')
+c1 = f.readline().split()
+rc = 0
+while c1 != []:
+    c = 0
+    for i in range(6):
+        if c1.count(c1[i])==1 and c1[i] in c2[i]:
+            c+=1
+    if c>3:
+        rc+=1
+    c1 = f.readline().split()
+print(rc)
